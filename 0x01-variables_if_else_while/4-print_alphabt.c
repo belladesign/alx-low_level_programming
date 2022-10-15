5ac35f9f763dbb899141122665fcf09e42fa2eb2
belladesign <estelleokafor@gmail.com>
2022-10-15 09:24:28 -0700

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,12 @@
  */
 int main(void)
 {
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 97; i < 123; i++)
+	for (i = 0; i < 24; i++)
 	{
-		if (i != 101 && != 113)
-		{
-			putchar(i);
-		}
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
