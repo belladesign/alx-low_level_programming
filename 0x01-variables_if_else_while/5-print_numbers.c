6635d17260c6c19bb14836ed85f79fd85db8dced
belladesign <estelleokafor@gmail.com>
2022-10-16 04:19:52 -0700

    C- variables_if_else_while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0  (Success)
  */
-int main{
+int main(void)
+{
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
 		printf("%d", i);
-	}
-	putchar('\n');
+
+	printf('\n');
+
 	return (0);
 }
