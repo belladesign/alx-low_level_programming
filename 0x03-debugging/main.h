01d7fce685ba7a288540feffd993576561d29b77
belladesign <estelleokafor@gmail.com>
2022-10-20 00:25:06 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _main_h_
+#define _main_h_
 
 #include <stdio.h>
 
-void positive_or_negative;
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+#endif /* _main_h_ */
